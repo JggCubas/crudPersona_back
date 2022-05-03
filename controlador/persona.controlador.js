3b21cd363befed04911ab943f08b9043e13bdd0b
cubas <ing.cubas@gmail.com>
2022-05-03 18:41:54 -0500

    {--}

--- controlador/persona.controlador.js
+++ controlador/persona.controlador.js
@@ -1,7 +1,51 @@
 const Personas = require("../modelo/persona.modelo.js");
 // Create and Save a new Tutorial
-exports.create = (req, res) => {
+var date = new Date();
+const formatDate = (date,sw)=>{
+  let formatted_date;
+  if(sw == 1){
+    formatted_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  }else{
+    formatted_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  }
+ return formatted_date;
+}
 
+
+exports.create = (req, res) => {
+  // Validate request
+    if (!req.body.nodo) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
+
+
+    const persona = new Personas({
+        Id : null,
+        Nombre : req.body.nodo.Nombre,
+        Ape_Pat : req.body.nodo.Ape_Pat,
+        Ape_Mat : req.body.nodo.Ape_Mat,
+        Rfc : req.body.nodo.Rfc,
+        Curp : req.body.nodo.Curp,
+        Fecha_Nacimiento : req.body.nodo.Fecha_Nacimiento || '',
+        Fecha_Alta : formatDate(date,1),
+        Fecha_Modificacion : formatDate(date,2),
+        Estatus_Id : req.body.nodo.Estatus_Id,
+        Sexo_id : req.body.nodo.Sexo_id || '',
+        Persona_Tipo_id : req.body.nodo.Persona_Tipo_id,
+        Avatar : req.body.nodo.Avatar || ''
+
+    });
+    // Save Tutorial in the database
+    Personas.create(persona, (err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Tutorial."
+        });
+      else res.send(data);
+    });
 };
 // Retrieve all Tutorials from the database (with condition).
 exports.findAll = (req, res) => {
@@ -15,24 +59,47 @@ exports.findAll = (req, res) => {
     else res.send(data);
   });
 };
-
-// // Find a single Tutorial with a id
-// exports.findOne = (req, res) => {
-//
-// };
-// // find all published Tutorials
-// exports.findAllPublished = (req, res) => {
-//
-// };
-// // Update a Tutorial identified by the id in the request
-// exports.update = (req, res) => {
-//
-// };
-// // Delete a Tutorial with the specified id in the request
-// exports.delete = (req, res) => {
-//
-// };
-// // Delete all Tutorials from the database.
-// exports.deleteAll = (req, res) => {
-//
-// };
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+  console.log(req);
+  Personas.remove(req.params.id, (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found Tutorial with id ${req.params.id}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Could not delete Tutorial with id " + req.params.Id
+          });
+        }
+      } else res.send({ message: `Tutorial was deleted successfully!` });
+    });
+};
+// Update a Tutorial identified by the id in the request
+exports.update = (req, res) => {
+  // Validate Request
+   if (!req.body.nodo) {
+     res.status(400).send({
+       message: "Content can not be empty!"
+     });
+   }
+   req.body.nodo.Fecha_Modificacion = formatDate(date,1);
+   Personas.updateById(
+     req.params.id,
+     new Personas(req.body.nodo),
+     (err, data) => {
+       if (err) {
+         if (err.kind === "not_found") {
+           res.status(404).send({
+             message: `Not found Tutorial with id ${req.params.id}.`
+           });
+         } else {
+           res.status(500).send({
+             message: "Error updating Tutorial with id " + req.params.id
+           });
+         }
+       } else res.send(data);
+     }
+   );
+};
--- modelo/persona.modelo.js
+++ modelo/persona.modelo.js
@@ -1,19 +1,28 @@
 const sql = require("../db/coneccion.js");
 // constructor
 const Personas = function(personas) {
-  this.title = personas.title;
-  this.description = personas.description;
-  this.published = personas.published;
+  this.Id = personas.Id;
+  this.Nombre = personas.Nombre;
+  this.Ape_Pat = personas.Ape_Pat;
+  this.Ape_Mat = personas.Ape_Mat;
+  this.Rfc = personas.Rfc;
+  this.Curp = personas.Curp;
+  this.Fecha_Nacimiento = personas.Fecha_Nacimiento;
+  this.Fecha_Alta = personas.Fecha_Alta;
+  this.Fecha_Modificacion = personas.Fecha_Modificacion;
+  this.Estatus_Id = personas.Estatus_Id;
+  this.Sexo_id = personas.Sexo_id;
+  this.Persona_Tipo_id = personas.Persona_Tipo_id;
+  this.Avatar = personas.Avatar;
 };
 Personas.create = (newPersona, result) => {
   sql.query("INSERT INTO Persona SET ?", newPersona, (err, res) => {
     if (err) {
-      console.log("error: ", err);
       result(err, null);
       return;
     }
-    console.log("created tutorial: ", { id: res.insertId, ...newTutorial });
-    result(null, { id: res.insertId, ...newTutorial });
+    newPersona.Id = res.insertId;
+    result(null, { ...newPersona });
   });
 };
 
@@ -27,5 +36,40 @@ Personas.getAll = (result) => {
     }
     result(null, res);
   });
+};
+Personas.remove = (id, result) => {
+  sql.query("DELETE FROM Persona WHERE Id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    if (res.affectedRows == 0) {
+      result({ kind: "not_found" }, null);
+      return;
+    }
+    console.log("deleted tutorial with id: ", id);
+    result(null, res);
+  });
+};
+Personas.updateById = (id, persona, result) => {
+
+
+  sql.query(
+    "UPDATE Persona SET Nombre = ?,Ape_Pat = ?,Ape_Mat = ?,Rfc = ?,Curp = ?,Fecha_Nacimiento = ?,Fecha_Modificacion = ?,Estatus_Id = ?,Sexo_id = ?,Persona_Tipo_id = ?,Avatar = ? WHERE id = ?",
+    [persona.Nombre,persona.Ape_Pat,persona.Ape_Mat,persona.Rfc,persona.Curp,persona.Fecha_Nacimiento,persona.Fecha_Modificacion,persona.Estatus_Id,persona.Sexo_id,persona.Persona_Tipo_id,persona.Avatar, id],
+    (err, res) => {
+      if (err) {
+        result(null, err);
+        return;
+      }
+      if (res.affectedRows == 0) {
+        // not found Tutorial with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      result(null, {  ...persona });
+    }
+  );
 };
 module.exports = Personas;
